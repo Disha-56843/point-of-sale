05cf340c9b50968906c4b48d6cda51a536f1bbbc
Disha-56843 <dishajasani56843@gmail.com>
2024-08-05 18:47:12 +0530

    add no data found

--- Script.js
+++ Script.js
@@ -1,5 +1,8 @@
 const cards = document.querySelectorAll('.card')
 
+const nodata = document.querySelector('.no-data')
+nodata.style.display = 'none'
+
 input.addEventListener('input', (event) => {
     cards.forEach((card) => {
         const name = card.querySelector('h3').innerText
@@ -7,9 +10,17 @@ input.addEventListener('input', (event) => {
 
         if (name.includes(event.target.value) || price.includes(event.target.value)) {
             card.style.display = 'block'
+            nodata.style.display = 'none'
 
             return
         }
+        if (card.style.display == 'none') {
+            nodata.style.display = 'block'
+            nodata.innerHTML = 'No Result Found'
+
+        }
         card.style.display = 'none'
+
+
     })
 })
