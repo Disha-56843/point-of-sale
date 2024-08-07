073cc9f01bd7c7c3148f80a10c33716fc78121dc
Disha-56843 <dishajasani56843@gmail.com>
2024-08-07 11:35:27 +0530

    sort product acording to their category

---
+++ script.js
@@ -0,0 +1,78 @@
+const product = document.querySelectorAll('.card')
+const nodata = document.querySelector('.no-data')
+const categoryItems = document.querySelectorAll('#category-list li')
+
+nodata.style.display = 'none'
+
+const input = document.querySelector('#input')
+input.value = localStorage.getItem('input-value') ?? ''
+
+function searchProducts(searchValue) {
+
+    let visibleProducts = false
+
+    product.forEach((card) => {
+        const pname = card.querySelector('.item-name').innerText
+        const price = card.querySelector('#price').innerText
+
+        if (pname.includes(searchValue) || price.includes(searchValue)) {
+            card.style.display = 'block'
+            visibleProducts = true
+
+            return
+        }
+
+        card.style.display = 'none'
+    })
+
+    if (!visibleProducts) {
+        nodata.style.display = 'block'
+
+
+        return
+    }
+
+    nodata.style.display = 'none'
+
+}
+
+searchProducts(input.value)
+
+input.addEventListener('input', (event) => {
+
+    const searchValue = event.target.value
+    localStorage.setItem('input-value', event.target.value)
+
+
+    searchProducts(searchValue)
+})
+
+
+function sortProducts(category , element) {
+
+    localStorage.setItem('selectedCategory', category);
+
+    product.forEach(card => {
+
+        if (category === 'all' || card.getAttribute('data-category') === category) {
+
+            card.style.display = 'block'
+
+            return
+        }
+
+        card.style.display = 'none'
+
+    })
+
+
+    categoryItems.forEach(items => {
+        items.classList.remove('select')
+    })
+
+    element.classList.add('select')
+
+
+
+}
+
